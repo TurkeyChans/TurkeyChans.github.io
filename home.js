163cf67b9e9a65dae4d8ba75ad6be184cb853f4b
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2024-01-14 23:19:22 -0500

    Add files via upload

--- home.js
+++ home.js
@@ -1,9 +1,144 @@
 var copyDate = new Date();
 year = copyDate.getFullYear();
 
-
 const copys = document.getElementById('copyrights');
-const copysss = document.createElement('p')
+const copysss = document.createElement('p');
 const copyss = document.createTextNode('Copyright ' + year);
 copysss.appendChild(copyss);
 copys.appendChild(copysss);
+
+let light_dark_mode = false;
+function both_dark_and_light(light_dark_mode) {
+    let imageElement = document.getElementById('discords');
+    let imageElement1 = document.getElementById('githubs');
+    if(light_dark_mode) {
+        let aElements = document.querySelectorAll('a');
+        for (let i = 0; i < aElements.length; i++) {
+            aElements[i].style.color = 'black';
+        }
+
+        let buttonElements = document.querySelectorAll('button');
+        for (let i = 0; i < buttonElements.length; i++) {
+            buttonElements[i].style.backgroundColor = 'white';
+        }
+
+        const switing = document.getElementById('switcher');
+        switing.innerHTML = 'Dark';
+        switing.style.color = 'black';
+        copys.style.color = 'black';
+        document.getElementById('switcher').innerHTML = 'Light';
+        document.body.style.backgroundColor = 'white';
+        
+        imageElement.src = 'darkdiscord.png';
+        imageElement1.src = 'darkgithub.png';
+    }
+    else {
+        let aElements = document.querySelectorAll('a');
+        for (let i = 0; i < aElements.length; i++) {
+            aElements[i].style.color = 'white';
+        }
+
+        let buttonElements = document.querySelectorAll('button');
+        for (let i = 0; i < buttonElements.length; i++) {
+            buttonElements[i].style.backgroundColor = 'black';
+        }
+        const switing = document.getElementById('switcher');
+        switing.innerHTML = 'Dark';
+        switing.style.color = 'white';
+        copys.style.color = 'white';
+        document.body.style.backgroundColor = "rgb(37, 37, 37)";
+        imageElement.src = 'lightdiscord.png';
+        imageElement1.src = 'lightgithub.png';
+    }
+}
+function dark_light_about_me() {
+    if(light_dark_mode) {
+        const dark_p = document.getElementById("about_me_p_id");
+        dark_p.style.color = 'black';
+        both_dark_and_light(true);
+        light_dark_mode = false;
+    }
+    else {
+        const dark_p = document.getElementById("about_me_p_id");
+        dark_p.style.color = 'white';
+        both_dark_and_light(false);
+        light_dark_mode = true;
+    }
+}
+function dark_light() {
+    var projectedit = 'project_edit' + 1;
+    var projectdate = 'project_date' + 1;
+    var projectdes = 'project_des' + 1;
+    var projects = 'projects' + 1;
+    if(light_dark_mode) {
+        const findings = document.getElementsByClassName('project1_dark').length;
+
+        for(let j = 1; j <= findings+1; ++j) {
+            const element3 = document.getElementById(projects);
+            element3.classList.replace("project1_dark", "project1");
+            projects = 'projects' + j
+        }
+
+        const findings2 = document.getElementsByClassName('project1').length;
+
+
+        for(let i = 1; i <= findings2+1; ++i) {
+            
+            const element = document.getElementById(projectedit);
+            element.classList.replace("project_edit_dark", "project_edit");
+            projectedit = 'project_edit' + i;
+            
+            const element1 = document.getElementById(projectdate);
+            element1.classList.replace("project_date_dark", "project_date");
+            projectdate = 'project_date' + i;
+            
+            const element2 = document.getElementById(projectdes);
+            element2.classList.replace("project-des_dark", "project-des");
+            projectdes = 'project_des' + i;  
+             
+        }
+        const element4 = document.getElementById("lable_proo");
+        element4.classList.replace("lable_pro_dark", "lable_pro");
+        
+        both_dark_and_light(true);
+        light_dark_mode = false;
+    }
+    
+    else {
+        const findings = document.getElementsByClassName('project1').length;
+
+        for(let j = 1; j <= findings+1; ++j) {
+
+            const element3 = document.getElementById(projects);
+            element3.classList.replace("project1", "project1_dark");
+            projects = 'projects' + j;
+
+        }
+
+        const findings2 = document.getElementsByClassName('project1_dark').length;
+
+
+        for(let i = 1; i <= findings2+1; ++i) {
+
+            const element = document.getElementById(projectedit);
+            element.classList.replace("project_edit", "project_edit_dark");
+            projectedit = 'project_edit' + i;
+
+            const element1 = document.getElementById(projectdate);
+            element1.classList.replace("project_date", "project_date_dark");
+            projectdate = 'project_date' + i;
+
+            const element2 = document.getElementById(projectdes);
+            element2.classList.replace("project-des", "project-des_dark");
+            projectdes = 'project_des' + i;  
+             
+        }
+        const element4 = document.getElementById("lable_proo");
+        element4.classList.replace("lable_pro", "lable_pro_dark");
+
+        
+        both_dark_and_light(false);
+        light_dark_mode = true;
+    }
+}
+
